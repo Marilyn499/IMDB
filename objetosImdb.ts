388142f5cf790ef162ca89920856dcacf1b5054c
Marilyn499 <marilyn.gutierrezc@outlook.com>
2020-10-25 19:57:13 +0100

    actualizacion hacer json

--- objetosImdb.ts
+++ objetosImdb.ts
@@ -1,8 +1,16 @@
+const fs = require('fs');
 import {Imdb} from "./claseImdb"
 import { Movie } from "./classMovie"
 
 let movie1:Movie = new Movie("Titac",1997,"EEUU","Drama");
 let movie2:Movie = new Movie("The Imitation Game",2014,"EEUU","Biopic")
 
-let prueba1:Imdb = new Imdb([movie1,movie2])
-prueba1.printAllMovies();
+let todasLasPeliculas:Imdb = new Imdb([movie1,movie2])
+todasLasPeliculas.printAllMovies();
+
+
+let json = JSON.stringify(todasLasPeliculas,null,2);
+fs.writeFileSync('imdbBBDD.json', json, function (err) {
+    if (err) throw err;
+    console.log('It\'s saved!');
+  });
